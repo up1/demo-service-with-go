3f2116b34eb5f86a3a859cfc39b9174d21792c0c
somkiat <somkiat.p@gmail.com>
2018-07-11 09:19:11 +0700

    Add hostname to code

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
-  "encoding/json"
-  "fmt"
+	"os"
 )
 
 type helloWorldResponse struct {
@@ -19,42 +20,43 @@ type bodyResponse struct {
 }
 
 type helloWorld2Response struct {
-  Message string `json:"message"`
+	Message string `json:"message"`
 }
 type helloWorld2Request struct {
-  Name string `json:"name"`
+	Name string `json:"name"`
 }
 
 func main() {
 	http.HandleFunc("/hello", helloHandler)
-  http.HandleFunc("/hello2", hello2Handler)
+	http.HandleFunc("/hello2", hello2Handler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func hello2Handler(w http.ResponseWriter, r *http.Request) {
-  // Request
-  var request helloWorld2Request
-  decoder := json.NewDecoder(r.Body)
-  err := decoder.Decode(&request)
-  if err != nil {
-     http.Error(w, "Bad request", http.StatusBadRequest)
-    return
-  }
-  // Response
-  response := helloWorld2Response{
-    Message: fmt.Sprintf("Hello %v", request.Name) }
-  encoder := json.NewEncoder(w)
-  encoder.Encode(&response)
+	// Request
+	var request helloWorld2Request
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&request)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	// Response
+	response := helloWorld2Response{
+		Message: fmt.Sprintf("Hello %v", request.Name)}
+	encoder := json.NewEncoder(w)
+	encoder.Encode(&response)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	name, _ := os.Hostname()
 	response := helloWorldResponse{
 		Header: headerResponse{
 			Code:        200,
 			Description: "Success",
 		},
-		Body: bodyResponse{Message: "Hello World"},
+		Body: bodyResponse{Message: fmt.Sprintf("Hello World %v", name)},
 	}
-  encoder := json.NewEncoder(w)
-  encoder.Encode(&response)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(&response)
 }
